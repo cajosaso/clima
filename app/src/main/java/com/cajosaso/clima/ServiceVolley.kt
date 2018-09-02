b1d6be3a2798738dced5cea74a99d3c103a34afc
sofiamorseletto <sofia.morseletto@mercadolibre.com>
2018-09-01 23:10:31 -0300

    Added service to GET requests from app server

---
+++ app/src/main/java/com/cajosaso/clima/ServiceVolley.kt
@@ -0,0 +1,35 @@
+package com.cajosaso.clima
+
+import android.util.Log
+import com.android.volley.AuthFailureError
+import com.android.volley.Response
+import com.android.volley.VolleyLog
+import com.android.volley.toolbox.JsonObjectRequest
+import org.json.JSONObject
+import java.util.HashMap
+
+class ServiceVolley : ServiceInterface {
+    val TAG = ServiceVolley::class.java.simpleName
+    val basePath = "https://your/backend/api/"
+
+    override fun get(path: String, completionHandler: (response: JSONObject?) -> Unit) {
+        val jsonObjReq = object : JsonObjectRequest(Method.GET, basePath + path, null,
+                Response.Listener<JSONObject> { response ->
+                    Log.d(TAG, "/get request OK! Response: $response")
+                    completionHandler(response)
+                },
+                Response.ErrorListener { error ->
+                    VolleyLog.e(TAG, "/get request fail! Error: ${error.message}")
+                    completionHandler(null)
+                }) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val headers = HashMap<String, String>()
+                headers.put("Content-Type", "application/json")
+                return headers
+            }
+        }
+
+        BackendVolley.instance?.addToRequestQueue(jsonObjReq, TAG)
+    }
+}
