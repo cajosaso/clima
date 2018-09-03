ba1b2a0ff0889b9919dcfcc2ab5f79f3de84d825
Santiago <santiago.v.gandolfo@gmail.com>
2018-09-03 11:32:39 -0300

    Added dummy request so that when there is no connection, it stays in the CitiesActivity and shows the Toast "no connection" message. It must receive a response in order to move to the WeatherActivity

--- app/src/main/java/com/cajosaso/clima/CitiesAdapter.kt
+++ app/src/main/java/com/cajosaso/clima/CitiesAdapter.kt
@@ -8,9 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.content.Intent
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
-
+import kotlinx.android.synthetic.main.activity_weather.*
 
 
 class CitiesAdapter(val context: Context, val cityList: ArrayList<City>): RecyclerView.Adapter<CitiesAdapter.CitiesViewHolder>() {
@@ -22,11 +24,32 @@ class CitiesAdapter(val context: Context, val cityList: ArrayList<City>): Recycl
         holder?.name?.text = cityList[position].name
         holder?.parentLayout.setOnClickListener { view ->
             Log.d(TAG, "onClick: clicked on: " + cityList[position].name);
-            Toast.makeText(context, cityList[position].name, Toast.LENGTH_SHORT).show()
 
-            val intent = Intent(context, WeatherActivity::class.java)
-            intent.putExtra("name", cityList[position].name)
-            context.startActivity(intent)
+
+            val service = ServiceVolley()
+            val apiController = APIController(service)
+
+            val path = "weather/" + cityList[position].name.replace(" ", "%20") + "/forecast"
+
+            apiController.get(path) { response ->
+                Log.d(TAG, response.toString())
+
+                if (response != null) {
+                    Toast.makeText(context, cityList[position].name, Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(context, WeatherActivity::class.java)
+                    intent.putExtra("name", cityList[position].name)
+                    context.startActivity(intent)
+                }
+
+                else {
+                    Toast.makeText(context, "No fue posible conectarse al servidor, por favor intente más tarde", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+
+
         }
     }
 
